b7a19cae0cc3147adfb1f61a410e60ec70c1dbe7
matteoga95 <gallo.work95@gmail.com>
2022-12-20 15:21:31 +0100

    milestone 1

---
+++ database/migrations/2022_12_20_135640_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 70)->nullable();
+            $table->string('stazione_di_partenza', 50)->nullable();
+            $table->string('stazione_di_arrivo', 50)->nullable();
+            $table->time('orario_di_partenza')->nullable();
+            $table->time('orario_di_arrivo')->nullable();
+            $table->string('codice_treno', 7);
+            $table->tinyInteger('numero_carrozze')->unsigned()->nullable();
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
